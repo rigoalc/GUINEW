6029ac1b42c4235401328b51338111ea6d55b55e
Rigo Alcover <rodigomartinalcover@gmail.com>
2022-02-14 12:12:58 -0500

    Pakages and virtual

--- test_ageleapyears.py
+++ test_ageleapyears.py
@@ -2,6 +2,20 @@
 # Program GUI 2/7/2022
 # CIS 226-23199 Advanced Python Programming
 
+'''
+On the terminal:
+
+python3 -m venv venv
+. venv/bin/activate
+pip install Flask
+export FLASK_APP=hello.py
+flask run
+* Running on http://127.0.0.1:5000/
+pip install pytest
+pytest
+ 
+'''
+
 from ageleapyears import age_leap_years
 
 def test_age_leap_years():
